1e9837804c8640957dcdc23a2d8a291c033875df
Alvaro Aledo <118385592+Saturno10@users.noreply.github.com>
2023-11-06 00:46:06 +0100

    Update DicPalabra.cpp

--- 004-DiccionarioDePalabrasConListas/src/DicPalabra.cpp
+++ 004-DiccionarioDePalabrasConListas/src/DicPalabra.cpp
@@ -1,18 +1,18 @@
-#include "../include/DicPalabra.h"
+#include "DicPalabra.h"
 
 
-
+//Constructor del diccionario
 DicPalabras::DicPalabras()
 {
     list<string> lista; //Almacena las palabras del diccionario
     contador = 0;
 }
-
+//Destructor (no necesario al ser un objeto estático)
 DicPalabras::~DicPalabras()
 {
     //dtor
 }
-
+//Elimina las palabras y resetea el contador
 void DicPalabras::vaciar(){
     //Eliminará todas las palabras del diccionario,
     //y se aplica con el comando <vaciar>
@@ -23,14 +23,8 @@ void DicPalabras::vaciar(){
 
 
 }
-
+//Añade una palabra, esta tiene que estar normalizada y ya en el intérprete nos aseguramos de que no está.
 void DicPalabras::insertar(string palabra) {
-    //Se insertará la palabra de forma ordenada en la lista (conjunto)(si no existe ya).
-    // El orden será el de la comparación entre string de C++. Las palabras
-    //insertadas deben estar normalizadas. Se usará dentro del comando <insertar>.
-
-    //N palabras que aparecen dentro del comando (aunque esten repetidas)
-    //M palabras palabras en el diccionario (No repetidas)
 
     this->lista.push_back(palabra);
 
@@ -38,11 +32,10 @@ void DicPalabras::insertar(string palabra) {
 
 
 }
-
+//Orden de búsqueda de palabras en la lista de un diccionario, la palabra se da normalizada
 bool DicPalabras::buscar(string palabra) {
-    //Busca una palabra dentro del diccionario, indicando si está o no.
-    //La palabra debe estar normalizada. Se usa en el comando <buscar>.
-    for (list<string>::iterator it = lista.begin(); it != lista.end(); ++it ){
+
+    for (list<string>::iterator it = this->lista.begin(); it != this->lista.end(); ++it ){
 
         if (*it == palabra) {
 
