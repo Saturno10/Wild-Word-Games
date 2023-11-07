f01d02c4ffcfaf48158b7d9c305f9fda8179772d
Alvaro Aledo <118385592+Saturno10@users.noreply.github.com>
2023-11-07 10:21:28 +0100

    Update DicPalabra.cpp

--- 004-DiccionarioDePalabrasConListas/src/DicPalabra.cpp
+++ 004-DiccionarioDePalabrasConListas/src/DicPalabra.cpp
@@ -25,17 +25,23 @@ void DicPalabras::vaciar(){
 }
 //Añade una palabra, esta tiene que estar normalizada y ya en el intérprete nos aseguramos de que no está.
 void DicPalabras::insertar(string palabra) {
+    list<string>::iterator it= lista.begin();
+    while (it!=lista.end() && *it<palabra)
+        it++;
+    if (it==lista.end() || *it!=palabra) {
+        lista.insert(it, palabra);
+        contador++;
+    }
+    /*this->lista.push_back(palabra);
 
-    this->lista.push_back(palabra);
-
-    this->contador++;
+    this->contador++;*/
 
 
 }
 //Orden de búsqueda de palabras en la lista de un diccionario, la palabra se da normalizada
 bool DicPalabras::buscar(string palabra) {
 
-    for (list<string>::iterator it = this->lista.begin(); it != this->lista.end(); ++it ){
+    /*for (list<string>::iterator it = this->lista.begin(); it != this->lista.end(); ++it ){
 
         if (*it == palabra) {
 
@@ -43,5 +49,10 @@ bool DicPalabras::buscar(string palabra) {
         }
     }
 
-    return false;
+    return false;*/
+
+    list<string>::iterator  it = this->lista.begin();
+    while (it!=this->lista.end() && *it < palabra) it++;
+    if(it==lista.end() || *it!=palabra) return false;
+    return true;
 }
