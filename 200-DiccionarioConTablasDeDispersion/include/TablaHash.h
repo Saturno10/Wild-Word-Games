336db58c95a8f5d6abf19f2c99846d67fbe3f771
Alvaro Aledo Tornero <118385592+Saturno10@users.noreply.github.com>
2023-11-26 14:35:37 +0100

    Update TablaHash.h

--- 200-DiccionarioConTablasDeDispersion/include/TablaHash.h
+++ 200-DiccionarioConTablasDeDispersion/include/TablaHash.h
@@ -5,24 +5,14 @@
 #include <list>
 
 using namespace std;
+const unsigned int TAMANO = 150001; //Tamaño primo para aumentar eficiencia y dispersión
+const int COPRIMO_TAMANO = 3;
 class TablaHash{
 private:
-//NUEVO
-    //list <string> T[TAM] -> DISPERSION ABIERTA DE TAMAÑO FIJO
-        //Cada elemento de la tabla es una lista de string
-        //CONSTRUCTOR: No hace falta se inicializa directamente en la cabecera.
 
-    //list <string> *T -> DISPERSION ABIERTA DE TAMAÑO VARIABLE
-        //Cada posicion de la tabla apunta a una lista de elementos string
-        //COSTRUCTOR: T_abierta= new list<string>[tamano_variable]
+    list<string> T[TAMANO];
 
-    //string *T --> DISPERSION CERRADA DE TAMAÑO VARIABLE
-        //Cada posicion de la tabla apunta a un elemento
-        //CONSTRUCTOR: T_cerrada = new string[tamano_variable];
-
-   const unsigned static int TAMANO_TABLA=150000;
-    list<string> T[TAMANO_TABLA]; //DISPERSION ABIERTA TAMANO FIJO
-    int nElem; //Numero elementos introducidos en la tabla
+    int nElem;
 
 public:
     TablaHash ();
@@ -30,7 +20,7 @@ public:
     void insertar (string palabra);
     bool consultar (string palabra);
     void vaciar (void);
-    //void reestructurar();
+    //void reestructurar(); No se usa por ser tamaño fijo
     int numElem(void) {return nElem;}
 
 };
