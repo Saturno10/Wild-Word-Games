fb94566e3acf33bd95aaa1bf605dc4fbd31989b7
Alvaro Aledo Tornero <118385592+Saturno10@users.noreply.github.com>
2023-11-26 14:34:48 +0100

    Update TablaHash.cpp
    Comentar funciones no necesarias.
    Ordenar código

--- 200-DiccionarioConTablasDeDispersion/src/TablaHash.cpp
+++ 200-DiccionarioConTablasDeDispersion/src/TablaHash.cpp
@@ -1,22 +1,20 @@
-//
-// Created by alvaro-linux on 11/11/23.
-//
-
 #include "../include/TablaHash.h"
 
-//------------------------NUEVO MARIA-----------------------
-// Función para calcular el MCD (Máximo Común Divisor) de dos números
-int calcularMCD(int a, int b) { //lO DEJO POR SI CAMBIAMOS EL TAMAÑO A MANO 
+
+
+//TABLA HASH DE DISPERSIÓN ABIERTA Y DE TAMAÑO FIJO
+// Función para calcular el MCD (Máximo Común Divisor) de dos números NO USADO POR SER TAMAÑO FIJO, REEMPLAZADO POR CONSTANTE)
+/*int calcularMCD(int a, int b) { //lO DEJO POR SI CAMBIAMOS EL TAMAÑO A MANO
     while (b != 0) {
         int temp = b;
         b = a % b;
         a = temp;
     }
     return a;
-}
+}*/
 
-// Función para encontrar el coprimo de un número dado
-int encontrarCoprimo(int numero) {
+// Función para encontrar el coprimo de un número dado (NO USADO POR SER TAMAÑO FIJO, REEMPLAZADO POR CONSTANTE)
+/*int encontrarCoprimo(int numero) {
     if (numero % 2 == 0) {
         // Si es par, devolvemos 1 para evitar problemas con el MCD
         return 1;
@@ -29,54 +27,31 @@ int encontrarCoprimo(int numero) {
     }
 
     return 1; // En caso de no encontrar otro coprimo
-}
+}*/
 
 unsigned int funcionDispersion(string& clave) {
     unsigned int hash = 0;
-    for (int i = 0; i < clave.size(); i++) {
-        unsigned int coprimo = encontrarCoprimo(TAMANO_TABLA);
-        hash = hash * coprimo + clave[i];
+    //unsigned int coprimo = encontrarCoprimo(TAMANO); Usado cuando había tamaño variable
+    for (unsigned long int i = 0; i < clave.size(); i++) {
+
+        hash = hash * COPRIMO_TAMANO + clave[i];
 
     }
-    return hash % TAMANO_TABLA;
+    return hash % TAMANO;
 }
 
 
 
 
-TablaHash::TablaHash() : /*list<string>T[TAMANO_TABLA], (Se inicializa en la cabecera)*/ nElem(0){}
+
+TablaHash::TablaHash() {nElem = 0;}
 
 
 
-TablaHash::~TablaHash(){
-   // delete[] T;
-   //NO ES NECESARIO
-        //No usamos memoria dinamica (porque es tabla de tamaño fijo)
-        //La memoria de contenedores STL borra la memoria estatica.
-}
+TablaHash::~TablaHash() = default;
 
 
-
-
-/*--------------------------ANTIGUO ALVARO, NO BORRADO-----------------
-
-unsigned long long int funcionDispersion(string palabra){
-    unsigned long long int hash=0;
-    for (unsigned int i : palabra) {
-        hash+=i*65537/104729;
-    }
-
-    return hash;
-}
-
-TablaHash::TablaHash() = default; //T(new string*[10]()),B(10),nElem(0){}
-
-
-
-TablaHash::~TablaHash(){
-
-}--------------------------------------------------------------------*/
-
+//Inserta un elemento en su cubeta correspondiente si no es encontrado
 void TablaHash::insertar (string palabra){
     int indice = funcionDispersion(palabra);
     list<string>::iterator it = T[indice].begin();
@@ -88,6 +63,8 @@ void TablaHash::insertar (string palabra){
         T[indice].insert(it,palabra);
     }
 }
+
+//Consulta si un elemento está en su cubeta (lista ordenada)
 bool TablaHash::consultar (string palabra){
     int indice = funcionDispersion(palabra);
     list<string>::iterator it = T[indice].begin();
@@ -98,27 +75,12 @@ bool TablaHash::consultar (string palabra){
     return true;
 
 }
+
+//Limpia todas las listas de la tabla y resetea su contador
 void TablaHash::vaciar (){
-    for (unsigned int i = 0; i < TAMANO_TABLA; i++) {
-        this->T[i].clear();
+    for (auto & i : this->T) {
+        i.clear();
     }
     nElem = 0;
 
 }
-
-/*void TablaHash::reestructurar() {
-    int nuevoIndice = B * 2;
-    string *nuevaT = new string[nuevoIndice]();
-    for (int i = 0; i < B; ++i) {
-        if (!T[i].empty()) {
-            int indice = funcionDispersion(T[i]) % (nuevoIndice);
-            while (!nuevaT[indice].empty()) {
-                indice = (indice + 1) % (nuevoIndice);
-            }
-            nuevaT[indice] = T[i];
-        }
-    }
-    delete[] this->T;
-    this->T = nuevaT;
-    this->B = nuevoIndice;
-}*/
